2c40549ffc139afc7aa47de55c1697d280c20c01
CHMEX <33424502+CHMEX@users.noreply.github.com>
2018-12-14 07:54:18 +0100

    Update rewards.php
    Add license Info

--- rewards.php
+++ rewards.php
@@ -58,6 +58,24 @@ echo json_encode($response);
 
 /*
 -- Calculation by James
+-- https://github.com/jl777/komodo/blob/master/src/komodo_interest.h
+
+/******************************************************************************
+ * Copyright © 2014-2018 The SuperNET Developers.                             *
+ *                                                                            *
+ * See the AUTHORS, DEVELOPER-AGREEMENT and LICENSE files at                  *
+ * the top-level directory of this distribution for the individual copyright  *
+ * holder information and the developer policies on copyright and licensing.  *
+ *                                                                            *
+ * Unless otherwise agreed in a custom licensing agreement, no part of the    *
+ * SuperNET software, including this file may be copied, modified, propagated *
+ * or distributed except according to the terms contained in the LICENSE file *
+ *                                                                            *
+ * Removal or modification of this copyright notice is prohibited.            *
+ *                                                                            *
+ ******************************************************************************/
+
+
 uint64_t komodo_interest(uint64_t nValue,uint32_t ntiptime,uint32_t tiptime)
 {
 int32_t minutes; uint64_t interest = 0;
